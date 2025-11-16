76c6d74e939e3808274dc321a0b9e7c6bc7885f6
yosshii8888 <138771166+yosshii8888@users.noreply.github.com>
2025-11-17 08:58:38 +0900

    Update speaker_aplay.py

--- speaker/speaker_aplay.py
+++ speaker/speaker_aplay.py
@@ -1,5 +1,7 @@
 #-*- cording: utf-8 -*-
 # スピーカー設定をPWM出力可能にしておく(/boot/firmware/config.txtの末尾に"dtoverlay=audremap,pins_12_13"を追加)
+# デバイスの番号を確認してaplayの1,0の"1"の方の番号を調整
+# 音量調節 -> ターミナルで alsamixer と入力，GPIOピンに対応するデバイスを選択して上下矢印キー
 
 import subprocess
 
